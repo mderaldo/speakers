3deab9bb9d63b8673b3d325055b3b221ac09dd4c
mderaldo <mellyderaldo@example>
2015-10-07 15:05:44 -0300

    Adding index page.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
-	return "Hello Speakers!"
+	user = {'name': 'Speaker'}
+	title = 'Home'
+	return render_template('index.html', title=title, user=user)
