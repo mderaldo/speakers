6794e8afac74edefd0c16b6b484ffb587e220f64
mderaldo <mellyderaldo@example>
2015-10-12 15:54:49 -0300

    Adding profile page.

--- app/views.py
+++ app/views.py
@@ -8,3 +8,22 @@ def index():
 	user = {'name': 'Speaker'}
 	title = 'Home'
 	return render_template('index.html', title=title, user=user)
+
+
+@app.route('/profile')
+def profile():
+	user = {'name': 'Speaker'}
+	title = 'Home'
+	talks = [
+		{ 
+            'name': 'Talk 1', 
+            'description': 'Beautiful day in Portland!', 
+            'duration': '10'
+        },
+        { 
+            'name': 'Talk 2', 
+            'description': 'The Avengers movie was so cool!', 
+            'duration': '10'
+        }
+	]
+	return render_template('profile.html', title=title, user=user, talks=talks)
